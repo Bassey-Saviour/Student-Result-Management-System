ede682cad89c74fdf2c262d78d2af4518a82a262
Saviour <basseysaviour230@gmail.com>
2025-12-06 07:55:36 +0100

    added port

--- cgi-bin/db_config.py
+++ cgi-bin/db_config.py
@@ -3,15 +3,17 @@ import os
 
 def get_connection():
     # Get database credentials from environment variables
-    # For local development, uses defaults; for Render, uses env vars
+    # For local development, uses defaults; for Render/Aiven, uses env vars
     host = os.getenv('DB_HOST', '')
     user = os.getenv('DB_USER', '')
     password = os.getenv('DB_PASSWORD', '')
     database = os.getenv('DB_NAME', '')
+    port = int(os.getenv('DB_PORT', ''))  # Default MySQL port is 3306
     
     return mysql.connector.connect(
         host=host,
         user=user,
         password=password,
-        database=database
+        database=database,
+        port=port
     )
